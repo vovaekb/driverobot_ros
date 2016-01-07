426698cb42b80b273c246110f4f6c36df947d32b
vladimir <vladimir@vladimir-K43SJ.(none)>
2016-01-07 17:22:47 +0100

    Add rosserial_arduino sketches for driverobot_ros

---
+++ control_move/control_move.ino
@@ -0,0 +1,81 @@
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Int16.h>
+
+using namespace std;
+
+/* Robot wheel pins */
+int m11 = 3;
+int m12 = 6;
+
+int m21 = 9;
+int m22 = 10;
+
+
+ros::NodeHandle nh;
+
+std_msgs::Int16 shift_msg;
+
+void turnLeft()
+{
+    // turn left
+    analogWrite(m21, 150); 
+    analogWrite(m22, 0);
+    analogWrite(m11, 0);
+    analogWrite(m12, 0);
+    delay(3000); 
+    goForward();
+}
+
+void turnRight()
+{
+    // turn right
+    analogWrite(m21, 0);
+    analogWrite(m22, 0);
+    analogWrite(m11, 150); 
+    analogWrite(m12, 0);
+    delay(3000); 
+    goForward();
+}
+
+void goForward()
+{
+    // go forward
+    analogWrite(m11, 150); 
+    analogWrite(m12, 0);
+    analogWrite(m21, 150); 
+    analogWrite(m22, 0);
+    
+    //delay(15000);
+}
+
+
+void messageCb(const std_msgs::UInt16& message) 
+{
+  int shift_val = int(message.data);
+  
+  shift_msg.data = shift_val;
+  
+  char* log_msg;
+  if(shift_val < 0) log_msg = "Left";
+  else log_msg = "Right";
+  
+  nh.loginfo(log_msg);
+}
+
+
+ros::Subscriber<std_msgs::UInt16> sub("line_shift", &messageCb);
+
+void setup()
+{
+  nh.initNode();
+  nh.subscribe(sub);
+  
+  Serial.begin(57600);
+}
+
+void loop()
+{
+  nh.spinOnce();
+  delay(100);
+}
---
+++ driverobot_teleop_twist/driverobot_teleop/driverobot_teleop.ino
@@ -0,0 +1,94 @@
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
+
+using namespace std;
+
+/* Robot wheel pins */
+
+int m11 = 3;
+int m12 = 6;
+
+int m21 = 9;
+int m22 = 10;
+
+
+ros::NodeHandle nh;
+
+void turnLeft()
+{
+    nh.loginfo("Turn left");
+    // turn left
+    analogWrite(m21, 150); 
+    analogWrite(m22, 0);
+    analogWrite(m11, 0);
+    analogWrite(m12, 0);
+    delay(3000); 
+    goForward();
+}
+
+void turnRight()
+{
+  nh.loginfo("Turn right");
+    // turn right
+    analogWrite(m21, 0);
+    analogWrite(m22, 0);
+    analogWrite(m11, 150); 
+    analogWrite(m12, 0);
+    delay(3000); 
+    goForward();
+}
+
+void goForward()
+{
+  nh.loginfo("Go forward");
+    // go forward
+    analogWrite(m11, 150); 
+    analogWrite(m12, 0);
+    analogWrite(m21, 150);
+    analogWrite(m22, 0);
+    
+    //delay(15000);
+}
+
+void stop()
+{
+    nh.loginfo("Stop");
+    analogWrite(m11, 0);
+    analogWrite(m12, 0);
+    analogWrite(m21, 0);
+    analogWrite(m22, 0);
+}
+
+
+void messageCb(const geometry_msgs::Twist& message) 
+{
+  geometry_msgs::Vector3 linear = message.linear;
+  float forward_vel = float(linear.x);
+  
+  if(forward_vel == 0) { stop(); return; }
+  
+  geometry_msgs::Vector3 angular = message.angular;
+  float ang_vel = float(angular.z);
+  
+  if(ang_vel > 0) { turnLeft(); }
+  else if(ang_vel < 0) { turnRight(); }
+  else { goForward(); }
+}
+
+
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &messageCb);
+
+void setup()
+{
+  nh.initNode();
+  nh.subscribe(sub);
+  
+  Serial.begin(57600);
+}
+
+void loop()
+{
+  nh.spinOnce();
+  delay(100);
+}
